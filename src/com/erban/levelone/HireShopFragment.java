0cbe098c4f665fbcea691267c330b5d17e4fbd6b
Qisen Tang <woaitqs@gmail.com>
2015-06-27 23:41:56 +0800

    add filter view

--- src/com/erban/levelone/HireShopFragment.java
+++ src/com/erban/levelone/HireShopFragment.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,6 +24,7 @@ import com.erban.util.ViewUtils;
 import com.erban.view.FilterView;
 import com.erban.view.HireAdapter;
 import com.erban.view.SingleItemAdapter;
+import com.erban.view.VerticalItemAdapter;
 import com.erban.view.SingleItemAdapter.ExecuteItem;
 import com.erban.volley.GsonRequest;
 import com.erban.volley.HttpUrls;
@@ -50,13 +53,13 @@ public class HireShopFragment extends BaseShopFragment {
             public void onSelected(int pos) {
                 switch (pos) {
                 case 0:
-                    bindDistances(filterListView);
+                    bindZoom(filterListView);
                     break;
                 case 1:
-                    bindDistances(filterListView);
+                    bindSalarey(filterListView);
                     break;
                 case 2:
-                    bindDistances(filterListView);
+                    bindType();
                     break;
                 default:
                     break;
@@ -65,22 +68,123 @@ public class HireShopFragment extends BaseShopFragment {
         });
     }
     
-    private void bindDistances(ListView listview) {
+    private void bindType() {
+        // bind disntances.
+        GridView gridView = new GridView(getActivity());
+        VerticalItemAdapter itemAdapter = new VerticalItemAdapter();
+        ExecuteItem item = new ExecuteItem();
+        item.title = "服务员";
+        item.icon = String.valueOf(R.drawable.fuwuyuan);
+        ExecuteItem item1 = new ExecuteItem();
+        item1.title = "销售";
+        item1.icon = String.valueOf(R.drawable.xiaoshou);
+        ExecuteItem item3 = new ExecuteItem();
+        item3.title = "司机";
+        item3.icon = String.valueOf(R.drawable.siji);
+        ExecuteItem item4 = new ExecuteItem();
+        item4.title = "技工";
+        item4.icon = String.valueOf(R.drawable.jigong);
+        ExecuteItem item6 = new ExecuteItem();
+        item6.title = "厨师";
+        item6.icon = String.valueOf(R.drawable.chushi);
+        ExecuteItem item7 = new ExecuteItem();
+        item7.title = "保安";
+        item7.icon = String.valueOf(R.drawable.baoan);
+        ExecuteItem item8 = new ExecuteItem();
+        item8.title = "客服";
+        item8.icon = String.valueOf(R.drawable.kefu);
+        ExecuteItem item9 = new ExecuteItem();
+        item9.title = "营业员";
+        item9.icon = String.valueOf(R.drawable.shouying);
+        ExecuteItem item10 = new ExecuteItem();
+        item10.title = "快递员";
+        item10.icon = String.valueOf(R.drawable.kuaidi);
+        ExecuteItem item11 = new ExecuteItem();
+        item11.title = "跟单员";
+        item11.icon = String.valueOf(R.drawable.gendan);
+        ExecuteItem item12 = new ExecuteItem();
+        item12.title = "财务";
+        item12.icon = String.valueOf(R.drawable.caowu);
+        ExecuteItem item13 = new ExecuteItem();
+        item13.title = "行政";
+        item13.icon = String.valueOf(R.drawable.xingzeng);
+        List<ExecuteItem> executeItems = new ArrayList<SingleItemAdapter.ExecuteItem>();
+        executeItems.add(item);
+        executeItems.add(item1);
+        executeItems.add(item3);
+        executeItems.add(item4);
+        executeItems.add(item6);
+        executeItems.add(item7);
+        executeItems.add(item8);
+        executeItems.add(item9);
+        executeItems.add(item10);
+        executeItems.add(item11);
+        executeItems.add(item12);
+        executeItems.add(item13);
+        itemAdapter.setItems(executeItems);
+        gridView.setAdapter(itemAdapter);
+        gridView.setGravity(Gravity.CENTER);
+        gridView.setNumColumns(4);
+        popupWindow.setContentView(gridView);
+        popupWindow.showAsDropDown(tabs.get(0));
+    }
+
+    private void bindSalarey(ListView listview) {
 
         // bind disntances.
         SingleItemAdapter itemAdapter = new SingleItemAdapter();
         ExecuteItem item = new ExecuteItem();
-        item.title = "海淀";
+        item.title = "不限";
         ExecuteItem item1 = new ExecuteItem();
-        item1.title = "朝阳";
+        item1.title = "面议";
         ExecuteItem item2 = new ExecuteItem();
-        item2.title = "东城";
+        item2.title = "2000元以下";
+        ExecuteItem item3 = new ExecuteItem();
+        item3.title = "2000-3000元";
+        ExecuteItem item4 = new ExecuteItem();
+        item4.title = "3000-5000元";
+        ExecuteItem item5 = new ExecuteItem();
+        item5.title = "5000-8000元";
         List<ExecuteItem> executeItems = new ArrayList<SingleItemAdapter.ExecuteItem>();
         executeItems.add(item);
         executeItems.add(item1);
         executeItems.add(item2);
+        executeItems.add(item3);
+        executeItems.add(item4);
+        executeItems.add(item5);
         itemAdapter.setItems(executeItems);
         listview.setAdapter(itemAdapter);
+        popupWindow.setContentView(filterListView);
+
+        popupWindow.showAsDropDown(tabs.get(0));
+    }
+    
+    private void bindZoom(ListView listview) {
+
+        // bind disntances.
+        SingleItemAdapter itemAdapter = new SingleItemAdapter();
+        ExecuteItem item = new ExecuteItem();
+        item.title = "全北京";
+        ExecuteItem item1 = new ExecuteItem();
+        item1.title = "海淀";
+        ExecuteItem item2 = new ExecuteItem();
+        item2.title = "朝阳";
+        ExecuteItem item3 = new ExecuteItem();
+        item3.title = "东城";
+        ExecuteItem item4 = new ExecuteItem();
+        item4.title = "崇文";
+        ExecuteItem item5 = new ExecuteItem();
+        item5.title = "丰台";
+        List<ExecuteItem> executeItems = new ArrayList<SingleItemAdapter.ExecuteItem>();
+        executeItems.add(item);
+        executeItems.add(item1);
+        executeItems.add(item2);
+        executeItems.add(item3);
+        executeItems.add(item4);
+        executeItems.add(item5);
+        itemAdapter.setItems(executeItems);
+        listview.setAdapter(itemAdapter);
+        popupWindow.setContentView(filterListView);
 
         popupWindow.showAsDropDown(tabs.get(0));
     }
